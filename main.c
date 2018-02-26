ab24ae2733487790c937ce7dad3a57a7953dd8da
Abel Tom <abeltom.kernel@gmail.com>
2018-02-26 19:06:55 +0100

    - Added ISR for timer, - removed delys, - added status register check

--- main.c
+++ main.c
@@ -7,17 +7,10 @@
 
 #include "usart.h"
 #include "spi_drv.h"
-
-#define ROM_SIZE 10
-unsigned char rom_contents[ROM_SIZE] = { 0x61, 0x62, 0x30, 0x31, 0x63, 0x64,
-		0x32, 0x33, 0x65, 0x66 };
-
-unsigned char Rx_rom_buff[ROM_SIZE] = { 0 };
+#include "mod_timer.h"
+#include "main.h"
 
 //static uint8_t rx_data;
-
-void eeprom_read(uint8_t start_addr, uint8_t *data_ptr, uint8_t size);
-void eeprom_write(uint8_t start_addr, uint8_t *data_ptr, uint8_t size);
 
 void eeprom_write(uint8_t start_addr, uint8_t *data_ptr, uint8_t size)
 {
@@ -26,7 +19,8 @@ void eeprom_write(uint8_t start_addr, uint8_t *data_ptr, uint8_t size)
 	for (i = 0; i < size; i++)
 	{
 		write_to_mem(start_addr + i, (*(data_ptr + i)));
-		_delay_us(2000);
+		while ((uint8_t) 0 != (read_status()))
+			;
 
 	}
 }
@@ -38,21 +32,26 @@ void eeprom_read(uint8_t start_addr, uint8_t *data_ptr, uint8_t size)
 	for (i = 0; i < size; i++)
 	{
 		*(data_ptr + i) = read_from_mem(start_addr + i);
-		_delay_us(200);
+
 	}
 }
 
 int main()
 {
+	sei();
+	DDRC |= 1 << DDC5;  // Set Pin PC5 as Output
+	PORTC |= 1 << PC5;  // Enable internal pull-up resistor PC5
+
+	OCR1A = 31250;      // Corresponds to foc=1Hz;
 
 	uint8_t *rom_ptr;
 	rom_ptr = &rom_contents[0];
 
+	timer_init();
+
 	usart_init(BAUD_VAL);
 
 	init_spi_master();
-
-	//spi_TxByte(0);
 
 	eeprom_write(0x00, rom_ptr, ROM_SIZE);
 
@@ -69,3 +68,10 @@ int main()
 
 	return 0;
 }
+
+ISR(TIMER1_COMPA_vect)
+{
+
+	PORTC ^= 1 << PC5; // Timepass
+}
+
